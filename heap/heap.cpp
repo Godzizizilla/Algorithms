f0df3e43b5b0ad576924892d80eea7a98c81c00b
Godzilla <2439396101@qq.com>
2023-07-10 14:37:36 +0800

    实现堆化算法(自顶向下/自底向上*最大堆/最小堆)

---
+++ heap/heap.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+
+using namespace std;
+
+void buMaxHeapify(int arr[], int n, int i) {
+    // 自下而上 (下滤)
+    // 该父结点是否比两个子结点都小 -> 是: 跟最大的子结点交换
+    // 交换之后它还有子结点, 递归地调用
+    int largest = i; // 最大元素的结点索引
+    int left = 2 * i + 1; // 左子结点索引
+    int right = 2 * i + 2; // 右子结点索引
+
+    // 比较父结点与左子结点的值 (先判断是否有左子结点-防止数组下标越界和结束递归)
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // 比较当前最大结点与右子结点的值 (先判断是否有右子结点-防止数组下标越界和结束递归)
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // 此时, largest就是最大值结点的索引(可能是父结点, 左子结点, 右子结点)
+    // 如果不是父结点最大, 那么交换位置, 并递归地调用
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        buMaxHeapify(arr, n, largest);
+    }
+}
+
+void buMinHeapify(int arr[], int n, int i) {
+    // 自下而上 (下滤)
+    // 该父结点是否比两个子结点都大 -> 是: 跟最小的子结点交换
+    // 交换之后它还有子结点, 递归地调用
+    int minimum = i; // 最小元素的结点索引
+    int left = 2 * i + 1; // 左子结点索引
+    int right = 2 * i + 2; // 右子结点索引
+
+    // 比较父结点与左子结点的值 (先判断是否有左子结点-防止数组下标越界和结束递归)
+    if (left < n && arr[left] < arr[minimum]) {
+        minimum = left;
+    }
+
+    // 比较当前最小结点与右子结点的值 (先判断是否有右子结点-防止数组下标越界和结束递归)
+    if (right < n && arr[right] < arr[minimum]) {
+        minimum = right;
+    }
+
+    // 此时, minimum就是最小值结点的索引(可能是父结点, 左子结点, 右子结点)
+    // 如果不是父结点最小, 那么交换位置, 并递归地调用
+    if (minimum != i) {
+        swap(arr[i], arr[minimum]);
+        buMinHeapify(arr, n, minimum);
+    }
+}
+
+void tdMaxHeapify(int arr[], int n, int i) {
+    // 自顶向下 (上滤)
+    int parent = (i - 1) / 2;
+
+    // 该结点是否比父结点大 -> 是: 交换, 并 *递归* 地执行
+    /*if (parent >= 0 && arr[i] > arr[parent]) {
+        swap(arr[i], arr[parent]);
+        tdMaxHeapify(arr, n, parent);
+    }*/
+
+    // 该结点是否比父结点大 -> 是: 交换, 并 *迭代* 地执行
+    while (parent >= 0 && arr[i] > arr[parent]) {
+        swap(arr[i], arr[parent]);
+        i = parent;
+        parent = (i - 1) / 2;
+    }
+}
+
+void tdMinHeapify(int arr[], int i) {
+    // 自顶向下 (上滤)
+    int parent = (i - 1) / 2;
+
+    // 该结点是否比父结点小 -> 是: 交换, 并 *递归* 地执行
+    /*if (parent >= 0 && arr[i] < arr[parent]) {
+        swap(arr[i], arr[parent]);
+        tdMinHeapify(arr, parent);
+    }*/
+
+    // 该结点是否比父结点小 -> 是: 交换, 并 *迭代* 地执行
+    while (parent >= 0 && arr[i] < arr[parent]) {
+        swap(arr[i], arr[parent]);
+        i = parent;
+        parent = (i - 1) / 2;
+    }
+}
+
+void buildHeap(int arr[], int n) {
+    // 自底向上: 从最后一个非叶子节点开始进行堆化
+    /*for (int i = n / 2 - 1; i >= 0; i--) {
+        buMinHeapify(arr, n, i);
+    }*/
+
+    // 自顶向下: 从第二个父结点开始进行堆化
+    for (int i = 1; i < n; i++) {
+        tdMaxHeapify(arr, n, i);
+    }
+}
+
+int main() {
+    int arr[] = {1, 5, 2, 7, 9, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    buildHeap(arr, n);
+
+    cout << "\nHeapified array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
